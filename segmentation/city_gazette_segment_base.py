184f8f373a056933c11b7dd01c2fbc3291fd3197
Jefersonalves <ferreira.jefersonn@gmail.com>
2023-10-14 15:16:20 -0300

    Cria classe base para o extrator de diários a partir dos textos do município

--- segmentation/city_gazette_segment_base.py
+++ segmentation/city_gazette_segment_base.py
@@ -42,60 +42,14 @@ class CityGazetteSegment:
     processed: bool
 
 
-class ALAssociacaoMunicipiosExtractor:
-    _mapa_meses = {
-        "Janeiro": 1,
-        "Fevereiro": 2,
-        "Março": 3,
-        "Abril": 4,
-        "Maio": 5,
-        "Junho": 6,
-        "Julho": 7,
-        "Agosto": 8,
-        "Setembro": 9,
-        "Outubro": 10,
-        "Novembro": 11,
-        "Dezembro": 12,
-    }
-
-    def get_city_segment(self, municipio: str, texto: str):
-        territory_name = municipio
-        source_text = texto.rstrip()
-        date = self._get_publication_date(texto)
-        edition_number = self._get_edition_number(texto)
-        is_extra_edition = False
-        power = "executive_legislative"
-        file_checksum = self.md5sum(BytesIO(self.source_text.encode(encoding='UTF-8')))
-        scraped_at = datetime.utcnow()
-        created_at = self.scraped_at
-        processed = True
-        
-        return CityGazetteSegment(
-            territory_name=territory_name,
-            source_text=source_text,
-            date=date,
-            edition_number=edition_number,
-            is_extra_edition=is_extra_edition,
-            power=power,
-            file_checksum=file_checksum,
-            scraped_at=scraped_at,
-            created_at=created_at,
-            processed=processed,
-        )
-
-    def _get_edition_number(self, texto: str) -> str:
-        match = re.search(r"Nº (\d+)", texto)
-        if match:
-            return match.group(1)
-        return None
-
-    def _get_publication_date(self, texto: str):
-        match = re.findall(
-            r".*(\d{2}) de (\w*) de (\d{4})", texto, re.MULTILINE)[0]
-        mes = self._mapa_meses[match[1]]
-        return date(year=int(match[2]), month=mes, day=int(match[0]))
-
-    def md5sum(self, file):
+class GazetteSegmentExtractor:
+    def __init__(self, city_text):
+        self.source_text = city_text
+    
+    def get_city_segment(self, city: str, city_text: str) -> CityGazetteSegment:
+        raise NotImplementedError
+    
+    def get_checksum(self, file):
         """Calculate the md5 checksum of a file-like object without reading its
         whole content in memory.
         from io import BytesIO
@@ -118,3 +72,56 @@ class ALAssociacaoMunicipiosExtractor:
 
     def __str__(self):
         return dict(self.__dict__)
+
+
+class ALAssociacaoMunicipiosExtractor(GazetteSegmentExtractor):
+    _mapa_meses = {
+        "Janeiro": 1,
+        "Fevereiro": 2,
+        "Março": 3,
+        "Abril": 4,
+        "Maio": 5,
+        "Junho": 6,
+        "Julho": 7,
+        "Agosto": 8,
+        "Setembro": 9,
+        "Outubro": 10,
+        "Novembro": 11,
+        "Dezembro": 12,
+    }
+
+    def get_city_segment(self, city: str, city_text: str):
+        territory_name = city
+        source_text = city_text.rstrip()
+        date = self._get_publication_date(city_text)
+        edition_number = self._get_edition_number(city_text)
+        is_extra_edition = False
+        power = "executive_legislative"
+        file_checksum = self.get_checksum(BytesIO(self.source_text.encode(encoding='UTF-8')))
+        utc_now = datetime.utcnow()
+        processed = True
+        
+        return CityGazetteSegment(
+            territory_name=territory_name,
+            source_text=source_text,
+            date=date,
+            edition_number=edition_number,
+            is_extra_edition=is_extra_edition,
+            power=power,
+            file_checksum=file_checksum,
+            scraped_at=utc_now,
+            created_at=utc_now,
+            processed=processed,
+        )
+
+    def _get_edition_number(self, texto: str) -> str:
+        match = re.search(r"Nº (\d+)", texto)
+        if match:
+            return match.group(1)
+        return None
+
+    def _get_publication_date(self, texto: str):
+        match = re.findall(
+            r".*(\d{2}) de (\w*) de (\d{4})", texto, re.MULTILINE)[0]
+        mes = self._mapa_meses[match[1]]
+        return date(year=int(match[2]), month=mes, day=int(match[0]))
